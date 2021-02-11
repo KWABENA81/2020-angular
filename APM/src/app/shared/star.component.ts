18e10a124862fc344fec2bcc6c1729f1278f0044
KWABENA81@YAHOO.COM <kwabena81@yahoo.com>
2021-02-10 19:43:12 -0500

    initial bulk APM

---
+++ APM/src/app/shared/star.component.ts
@@ -0,0 +1,25 @@
+
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+
+@Component({
+    selector: 'pm-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+
+export class StarComponent implements OnInit {
+
+    @Input() rating: number;
+    starWidth: number;
+
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
+    ngOnInit(): void {
+        this.starWidth = this.rating * 75 / 5;
+    }
+
+    onClick(): void {
+        this.ratingClicked.emit(`The rating is ${this.rating}`);
+    }
+
+}
